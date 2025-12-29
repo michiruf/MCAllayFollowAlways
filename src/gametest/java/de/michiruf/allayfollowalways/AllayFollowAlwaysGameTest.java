0a9cd93468d9adc1f96bad58cd5cc3ae40f5954d
Michael Ruf <git@michiruf.de>
2025-12-29 13:37:39 +0100

    Add proper tests that actually test the behaviour of the mod

--- src/gametest/java/de/michiruf/allayfollowalways/AllayFollowAlwaysGameTest.java
+++ src/gametest/java/de/michiruf/allayfollowalways/AllayFollowAlwaysGameTest.java
@@ -1,12 +1,12 @@
 package de.michiruf.allayfollowalways;
 
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.test.GameTest;
-import net.minecraft.test.TestContext;
+import net.fabricmc.fabric.api.entity.FakePlayer;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
 import net.minecraft.entity.passive.AllayEntity;
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.GameMode;
 
 import java.util.Set;
@@ -14,56 +14,68 @@ import java.util.Set;
 @SuppressWarnings("unused")
 public class AllayFollowAlwaysGameTest {
 
-    @GameTest(templateName = "fabric-gametest-api-v1:empty")
-    public void testTeleport(TestContext context) {
-        assert AllayFollowAlwaysMod.CONFIG.teleportEnabled() : "Teleportation must be enabled for this test";
+    private FakePlayer player;
+    private AllayEntity allay;
 
-        var player = context.createMockPlayer(GameMode.SURVIVAL);
-        var allay = context.spawnEntity(
-                net.minecraft.entity.EntityType.ALLAY,
-                new BlockPos(0, 1, 0)
-        );
+    private void createPlayerAndAllay(TestContext context) {
+        player = FakePlayer.get(context.getWorld());
+        player.changeGameMode(GameMode.SURVIVAL);
+
+        // Register player in the entity registry, necessary for lookups with uuid
+        context.getWorld().onPlayerConnected(player);
+
+        allay = context.spawnEntity(EntityType.ALLAY, new BlockPos(0, 1, 0));
+
+        // Teleport player to allay, because the player is not spawned in the test space
+        player.teleport(context.getWorld(), allay.getX(), allay.getY(), allay.getZ(), Set.of(), 0.0f, 0.0f, false);
+
+        // Fake that allay got an item from the player
         allay.getBrain().remember(MemoryModuleType.LIKED_PLAYER, player.getUuid());
+    }
 
-        Vec3d initialAllayPos = allay.getPos();
-        player.teleport(context.getWorld(), 100.0, 1.0, 0.0, Set.of(), 0.0f, 0.0f, false);
-        context.waitAndRun(1000, () -> {
-            double distanceToPlayer = allay.getPos().distanceTo(player.getPos());
-            assert distanceToPlayer <= 10.0 : "Allay is not close to player after teleport. Distance: " + distanceToPlayer;
+    private void clearPlayerAndAllay(TestContext context) {
+        context.killEntity(player);
+        context.killEntity(allay);
+        player = null;
+        allay = null;
+    }
 
+    @GameTest(templateName = "fabric-gametest-api-v1:empty", skyAccess = true)
+    public void testTeleport(TestContext context) {
+        AllayFollowAlwaysMod.CONFIG.teleportEnabled(true);
+        createPlayerAndAllay(context);
+
+        var destinationY = player.getY() + AllayFollowAlwaysMod.CONFIG.teleportDistance() + 10;
+        player.teleport(context.getWorld(), player.getX(), destinationY, player.getZ(), Set.of(), 0.0f, 0.0f, false);
+        context.waitAndRun(10, () -> {
+            var distanceToPlayer = allay.getPos().distanceTo(player.getPos());
+            context.assertTrue(distanceToPlayer <= 2.0, "Allay is not close to player after teleport. Distance: " + distanceToPlayer + ", Player: " + player.getBlockPos() + ", Allay: " + allay.getBlockPos());
+
+            clearPlayerAndAllay(context);
             context.complete();
         });
     }
 
     @GameTest(templateName = "fabric-gametest-api-v1:empty")
-    public void testDisabled(TestContext context) {
-        try {
-            AllayFollowAlwaysMod.CONFIG.teleportEnabled(false);
+    public void testTeleportDisabled(TestContext context) {
+        AllayFollowAlwaysMod.CONFIG.teleportEnabled(false);
+        createPlayerAndAllay(context);
 
-            var player = context.createMockPlayer(GameMode.SURVIVAL);
-            AllayEntity allay = context.spawnEntity(
-                    net.minecraft.entity.EntityType.ALLAY,
-                    new BlockPos(0, 1, 0)
-            );
-            allay.getBrain().remember(MemoryModuleType.LIKED_PLAYER, player.getUuid());
+        var destinationY = player.getY() + AllayFollowAlwaysMod.CONFIG.teleportDistance() + 10;
+        player.teleport(context.getWorld(), player.getX(), destinationY, player.getZ(), Set.of(), 0.0f, 0.0f, false);
+        context.waitAndRun(10, () -> {
+            var distanceToPlayer = allay.getPos().distanceTo(player.getPos());
+            context.assertFalse(distanceToPlayer <= 2.0, "Allay is too close to player and did teleport. Distance: " + distanceToPlayer + ", Player: " + player.getBlockPos() + ", Allay: " + allay.getBlockPos());
 
-            Vec3d initialAllayPos = allay.getPos();
-            player.teleport(context.getWorld(), 100.0, 1.0, 0.0, Set.of(), 0.0f, 0.0f, false);
-            context.waitAndRun(10, () -> {
-                double distanceMoved = allay.getPos().distanceTo(initialAllayPos);
-                assert distanceMoved > 10.0 : "Allay teleported when disabled! Distance moved: " + distanceMoved;
-
-                context.complete();
-            });
-        } finally {
-            AllayFollowAlwaysMod.CONFIG.teleportEnabled(true);
-        }
+            clearPlayerAndAllay(context);
+            context.complete();
+        });
     }
 
     @GameTest(templateName = "fabric-gametest-api-v1:empty")
     public void testMessages(TestContext context) {
         var player = context.createMockPlayer(GameMode.CREATIVE);
-        var commandSource = player.getCommandSource((ServerWorld) player.getEntityWorld());
+        var commandSource = player.getCommandSource(context.getWorld());
         context.getWorld().getServer().getCommandManager().executeWithPrefix(
                 commandSource,
                 "/allayfollowalways teleportEnabled"
