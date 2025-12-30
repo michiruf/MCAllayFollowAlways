d2864f1a213ece8e2221a0d978c1656c09535d72
Michael Ruf <git@michiruf.de>
2025-12-30 20:07:52 +0100

    Restructure game tests

---
+++ src/gametest/1.19/de/michiruf/allayfollowalways/AllayFollowAlwaysGameTest.java
@@ -0,0 +1,22 @@
+package de.michiruf.allayfollowalways;
+
+import net.minecraft.test.GameTest;
+import net.minecraft.test.TestContext;
+
+@SuppressWarnings("unused")
+public class AllayFollowAlwaysGameTest {
+
+    // Since fabric has no fake player in versions until 1.19.4, we cannot test the teleport behavior
+    // We need the fake player, because mocked players are not registered properly
+    // However, with this test being present, the mod will be installed and at least checked somehow
+
+    @GameTest(templateName = "fabric-gametest-api-v1:empty")
+    public void testMessages(TestContext context) {
+        var player = context.createMockCreativePlayer();
+        context.getWorld().getServer().getCommandManager().executeWithPrefix(
+                player.getCommandSource(),
+                "/allayfollowalways teleportEnabled"
+        );
+        context.complete();
+    }
+}
