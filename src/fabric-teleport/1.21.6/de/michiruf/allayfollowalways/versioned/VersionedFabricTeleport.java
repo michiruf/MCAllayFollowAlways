41379732c2327b8a8bda8a291dce8c44db1e54a4
Michael Ruf <git@michiruf.de>
2025-12-31 02:13:03 +0100

    Add support for 1.21.6

---
+++ src/fabric-teleport/1.21.6/de/michiruf/allayfollowalways/versioned/VersionedFabricTeleport.java
@@ -0,0 +1,35 @@
+package de.michiruf.allayfollowalways.versioned;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.AllayEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.TeleportTarget;
+
+/**
+ * @author Michael Ruf
+ * @since 2023-12-18
+ */
+public class VersionedFabricTeleport {
+
+    public static void teleport(AllayEntity allay, ServerPlayerEntity player) {
+        teleport(allay, player, player.getWorld());
+    }
+
+    public static void teleport(Entity entity, Entity to, ServerWorld world) {
+        teleport(entity, to.getPos(), world);
+    }
+
+    public static void teleport(Entity entity, Vec3d to, ServerWorld world) {
+        var target = new TeleportTarget(
+                world,
+                to,
+                entity.getVelocity(),
+                entity.getYaw(),
+                entity.getPitch(),
+                TeleportTarget.NO_OP
+        );
+        entity.teleportTo(target);
+    }
+}
