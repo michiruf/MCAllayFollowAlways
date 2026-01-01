9fb2ad4688d42b37c1ff653556b2dc9b98fd049d
Michael Ruf <git@michiruf.de>
2026-01-01 17:18:59 +0100

    Fix example game test

--- src/gametest/java/de/michiruf/allayfollowalways/ExampleGameTest.java
+++ src/gametest/java/de/michiruf/allayfollowalways/ExampleGameTest.java
@@ -1,12 +1,20 @@
 package de.michiruf.allayfollowalways;
 
-import net.minecraft.test.GameTest;
+//? if <=1.21.5 {
+/*import net.minecraft.test.GameTest;
+ *///? } else {
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
+//?}
 import net.minecraft.test.TestContext;
 
 @SuppressWarnings("unused")
 public class ExampleGameTest {
 
-    @GameTest(templateName = "fabric-gametest-api-v1:empty")
+    /*? if <1.21.5 { */
+    /*@GameTest(templateName = "fabric-gametest-api-v1:empty")
+    *//*?} else { */
+    @GameTest
+    /*?} */
     public void test(TestContext context) {
         context.complete();
     }
