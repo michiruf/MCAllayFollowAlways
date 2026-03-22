068e2f8f87e5440b967f20920cd370f1e2555250
Michael Ruf <git@michiruf.de>
2026-03-22 02:23:48 +0100

    Fix initialization bug, where no config existed and the logger tried to access it

--- src/main/java/de/michiruf/allayfollowalways/helper/ModLogger.java
+++ src/main/java/de/michiruf/allayfollowalways/helper/ModLogger.java
@@ -1,11 +1,12 @@
 package de.michiruf.allayfollowalways.helper;
 
 import de.michiruf.allayfollowalways.AllayFollowAlwaysMod;
+import de.michiruf.allayfollowalways.config.ConfigWrapper;
 import de.michiruf.allayfollowalways.config.LogLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.function.Supplier;
+import java.util.function.Function;
 
 /**
  * Purpose-based logger with per-category log levels.
@@ -25,15 +26,15 @@ public class ModLogger {
     }
 
     public void mod(LogLevel level, String msg, Object... args) {
-        log(AllayFollowAlwaysMod.CONFIG::logMod, level, msg, args);
+        log(ConfigWrapper::logMod, level, msg, args);
     }
 
     public void teleport(LogLevel level, String msg, Object... args) {
-        log(AllayFollowAlwaysMod.CONFIG::logTeleport, level, msg, args);
+        log(ConfigWrapper::logTeleport, level, msg, args);
     }
 
     public void leash(LogLevel level, String msg, Object... args) {
-        log(AllayFollowAlwaysMod.CONFIG::logLeash, level, msg, args);
+        log(ConfigWrapper::logLeash, level, msg, args);
     }
 
     public void warn(String msg, Object... args) {
@@ -44,9 +45,10 @@ public class ModLogger {
         delegate.error(msg, args);
     }
 
-    private void log(Supplier<LogLevel> configLevel, LogLevel required, String msg, Object... args) {
-        if (AllayFollowAlwaysMod.CONFIG != null
-                && configLevel.get().isEnabled(required))
+    private void log(Function<ConfigWrapper, LogLevel> configLevel, LogLevel required, String msg, Object... args) {
+        var config = AllayFollowAlwaysMod.CONFIG;
+        // In case there is no config yet, just log everything
+        if (config == null || configLevel.apply(config).isEnabled(required))
             delegate.info(msg, args);
     }
 }
